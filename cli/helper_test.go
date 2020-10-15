d3fc8332f2cc190865e8e5a68852e4b0595f8deb
Tomoya Kabe <limit.usus@gmail.com>
2020-10-15 16:07:05 +0900

    Add Version flag (#7)

---
+++ cli/helper_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"sync"
+)
+
+func captureOutput(f func()) string {
+	reader, writer, err := os.Pipe()
+	if err != nil {
+		panic(err)
+	}
+	stdout := os.Stdout
+	stderr := os.Stderr
+	defer func() {
+		os.Stdout = stdout
+		os.Stderr = stderr
+		// stdlog.SetOutput(os.Stderr)
+	}()
+	os.Stdout = writer
+	os.Stderr = writer
+	// stdlog.SetOutput(writer)
+	out := make(chan string)
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	go func() {
+		var buf bytes.Buffer
+		wg.Done()
+		io.Copy(&buf, reader)
+		out <- buf.String()
+	}()
+	wg.Wait()
+	f()
+	writer.Close()
+	return <-out
+}
